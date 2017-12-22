c36be940b60569510250d13a2d4b1116206bfea0
Ron Martin <ronmartin89@gmail.com>
2017-12-22 04:51:18 -0800

    Refactored code to be more modular. Created a very use snippet of code called 'create_folder' that prompts for a folder name and then creates that folder unless a name is passed to it when called (ie create_folder('homework').

--- Autorrent/__init__.py
+++ Autorrent/__init__.py
@@ -2,49 +2,64 @@ import os
 
 
 def creation(links='TEMP.txt'):
+
     torrents = open('torrents.txt', 'r').readlines()
-    curr_dir = os.getcwd()
-    torr_dir_name = 'test_torrent_directory'
-    
-    # Get the root directory
-    curr_dir = curr_dir[0:3]
-    try:
-        os.mkdir(curr_dir + torr_dir_name)
-    except FileExistsError:
-        print('That file already exists.')
-        name_folder()
-        # create folder save location
-        # utorrent api - set default save location
-        # parse link and put in api friendly format
-        # utorrent api - send "start job" request
-        # "          " - pause torrent (may not be needed)
-        # change default save location, reparse, and send now
-        #     "start job" request
-class CreateFolder(name='Torrent_Folder'):
-    curr_dir = os.getcwd()
-    # Gets root path (ie "C:/" or "C:\")
-    curr_dir = curr_dir[0:3]
-    try:
-        name_directory('What would you like to name the folder where everything will be saved?')
-        # f_name = input('What would you like to name the folder where everything will be saved?')
-    except Exception as e:
-        print(e)
-    else:
-        print('worked!')
+    torr_dir_name = None
+
+    create_folder()
+    # create_folder('TORRENT_NAME_HERE')
+
+    # OUTLINE
+    #
+    #  create folder save location
+    # utorrent api - set default save location
+    # parse link and put in api friendly format
+    # utorrent api - send "start job" request
+    # "          " - pause torrent (may not be needed)
+    # change default save location, reparse, and send now
+    #     "start job" request
 
 
-    def name_directory(question='DEFAULT QUESTION',):
+def create_folder(name=None):
+
+    def main():
+        curr_dir = os.getcwd()
+        # Gets root path (ie "C:/" or "C:\")
+        curr_dir = curr_dir[0:3]
+
+        QUESTION = 'What would you like to name the folder where everything will be saved? '
+        SUCCESS_REPLY = 'Folder will be named: '
+        try:
+            if name is not None:
+                folder_name = name
+            elif name is None:
+                folder_name = name_directory(QUESTION, SUCCESS_REPLY)
+            os.mkdir(curr_dir + folder_name)
+        except FileExistsError:
+            print('That file already exists.')
+            raise
+        except Exception as e:
+            print(e)
+        else:
+            print('Folder created:', curr_dir + folder_name)
+
+    def name_directory(question='DEFAULT QUESTION', success_reply=None):
         try:
             f_name = input(question)
-        except 
-        
+        except Exception as e:
+            print(e)
+        else:
+            if success_reply is not None:
+                print(success_reply, f_name)
+            return f_name
+        finally:
+            pass
 
-
-
+    main()
 
 
 def cleanup(thing):
     pass
 
-#creation()
-name_folder()
+
+creation()

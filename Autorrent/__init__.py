a70c37b8ef6f311b65eb02ca2c85a0d5af802dbe
Ron Martin <ronmartin89@gmail.com>
2017-12-22 02:27:42 -0800

    Figuring out the structure and created an example 'torrents.txt' to test against.

--- Autorrent/__init__.py
+++ Autorrent/__init__.py
@@ -1,8 +1,22 @@
+import os
 
 
-def creation(links):
-	pass
+def creation(links='TEMP.txt'):
+	torrents = open('torrents.txt', 'r').readlines()
+	#print(torrents)
+	curr_dir = os.getcwd()
+	print(curr_dir)
+	#os.mkdir('~\test_directory')
+        # create folder save location
+        # utorrent api - set default save location
+        # parse link and put in api friendly format
+        # utorrent api - send "start job" request
+        # "          " - pause torrent (may not be needed)
+        # change default save location, reparse, and send now
+        #     "start job" request
 
 
 def cleanup(thing):
 	pass
+
+creation()

a24840d68789b6696d8936912e68f24d309a1b44
Ron Martin <ronmartin89@gmail.com>
2017-12-22 03:21:45 -0800

    Refactoring using OOP.

--- Autorrent/__init__.py
+++ Autorrent/__init__.py
@@ -2,11 +2,17 @@ import os
 
 
 def creation(links='TEMP.txt'):
-	torrents = open('torrents.txt', 'r').readlines()
-	#print(torrents)
-	curr_dir = os.getcwd()
-	print(curr_dir)
-	#os.mkdir('~\test_directory')
+    torrents = open('torrents.txt', 'r').readlines()
+    curr_dir = os.getcwd()
+    torr_dir_name = 'test_torrent_directory'
+    
+    # Get the root directory
+    curr_dir = curr_dir[0:3]
+    try:
+        os.mkdir(curr_dir + torr_dir_name)
+    except FileExistsError:
+        print('That file already exists.')
+        name_folder()
         # create folder save location
         # utorrent api - set default save location
         # parse link and put in api friendly format
@@ -14,9 +20,31 @@ def creation(links='TEMP.txt'):
         # "          " - pause torrent (may not be needed)
         # change default save location, reparse, and send now
         #     "start job" request
+class CreateFolder(name='Torrent_Folder'):
+    curr_dir = os.getcwd()
+    # Gets root path (ie "C:/" or "C:\")
+    curr_dir = curr_dir[0:3]
+    try:
+        name_directory('What would you like to name the folder where everything will be saved?')
+        # f_name = input('What would you like to name the folder where everything will be saved?')
+    except Exception as e:
+        print(e)
+    else:
+        print('worked!')
+
+
+    def name_directory(question='DEFAULT QUESTION',):
+        try:
+            f_name = input(question)
+        except 
+        
+
+
+
 
 
 def cleanup(thing):
-	pass
+    pass
 
-creation()
+#creation()
+name_folder()
